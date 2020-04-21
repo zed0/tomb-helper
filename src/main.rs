4428749e7ec438cbcc54975baf5b745331b8e7a1
Ben Falconer <ben@falconers.me.uk>
2020-04-21 23:38:04 +0100

    feat: Move in all directions, toggle to activate/deactivate

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,159 @@
 use livesplit_hotkey::{Hook, KeyCode};
-use std::thread;
-use std::sync::mpsc;
+use process_memory::{DataMember, Memory, Pid, ProcessHandle, TryIntoProcessHandle};
 use std::ptr::{null, null_mut};
-use process_memory::{Memory, Pid, DataMember, TryIntoProcessHandle};
+use std::sync::mpsc;
 
 #[cfg(windows)]
 extern crate winapi;
 
 fn main() {
+    let keys = vec![
+        (KeyCode::F5, Action::ToggleActive),
+        (KeyCode::W, Action::Forward),
+        (KeyCode::S, Action::Backward),
+        (KeyCode::A, Action::Left),
+        (KeyCode::D, Action::Right),
+        (KeyCode::Space, Action::Up),
+        (KeyCode::C, Action::Down),
+    ];
+
     println!("Attaching to ROTTR.exe...");
     let (tx, rx) = mpsc::channel();
 
-    let memory_thread = thread::spawn(move || {
-        let pid = get_pid("ROTTR.exe");
-        let handle = pid.try_into_process_handle().unwrap();
-        println!("Found ROTTR.exe PID: {}", pid);
-        let base_addr = get_base_address(pid) as *const _ as usize;
-        println!("Found base memory address: 0x{:X}", base_addr);
-
-        let current_z = DataMember::<f32>::new_offset(handle, vec![base_addr + 0x01_08_2A_B8, 0x18]);
-	let mut current_z = current_z.read().unwrap();
-	loop {
-	    let direction = rx.try_recv();
-	    match direction {
-	        Ok("up") => {
-		    current_z += 100.0;
-                }
-	        Ok("down") => {
-		    current_z -= 100.0;
-                }
-                _ => {
-		}
-	    }
-            let z = DataMember::<f32>::new_offset(handle, vec![base_addr + 0x01_08_2A_B8, 0x18]);
-            z.write(&current_z);
-	}
-    });
-
     let hook = Hook::new().unwrap();
+    for (key, action) in keys.clone() {
+        let current_tx = tx.clone();
+        hook.register(key, move || {
+            current_tx.send(action).unwrap();
+        })
+        .unwrap();
+    }
 
-    let up_tx = tx.clone();
-    hook.register(KeyCode::Space, move || {
-        up_tx.send("up").unwrap();
-    }).unwrap();
+    let pid = get_pid("ROTTR.exe");
+    let handle = pid.try_into_process_handle().unwrap();
+    println!("Found ROTTR.exe PID: {}", pid);
+    let base_addr = get_base_address(pid) as *const _ as usize;
+    println!("Found base memory address: 0x{:X}", base_addr);
 
-    let down_tx = tx.clone();
-    hook.register(KeyCode::C, move || {
-        down_tx.send("down").unwrap();
-    }).unwrap();
+    let mut active = false;
+    let mut position = Position {
+        x: TrackedMemory {
+            data: 0.0,
+            offsets: vec![base_addr + 0x01_08_2A_B8, 0x10],
+        },
+        y: TrackedMemory {
+            data: 0.0,
+            offsets: vec![base_addr + 0x01_08_2A_B8, 0x14],
+        },
+        z: TrackedMemory {
+            data: 0.0,
+            offsets: vec![base_addr + 0x01_08_2A_B8, 0x18],
+        },
+    };
 
     println!("Started!");
-    thread::park();
-    hook.unregister(KeyCode::Space).unwrap();
+    print_help(&keys);
+
+    loop {
+        let signal = rx.try_recv();
+        if active {
+            match signal {
+                Ok(Action::ToggleActive) => {
+                    active = false;
+                    println!("deactivated");
+                }
+                Ok(Action::Forward) => position.x.data += 100.0,
+                Ok(Action::Backward) => position.x.data -= 100.0,
+                Ok(Action::Left) => position.y.data += 100.0,
+                Ok(Action::Right) => position.y.data -= 100.0,
+                Ok(Action::Up) => position.z.data += 100.0,
+                Ok(Action::Down) => position.z.data -= 100.0,
+                _ => {}
+            }
+
+            position.apply_to_game(handle);
+        } else {
+            match signal {
+                Ok(Action::ToggleActive) => {
+                    active = true;
+                    position.fetch_from_game(handle);
+                    println!("activated");
+                }
+                _ => {}
+            }
+        }
+    }
 }
 
+#[derive(Debug, Copy, Clone)]
+enum Action {
+    ToggleActive,
+    Forward,
+    Backward,
+    Left,
+    Right,
+    Up,
+    Down,
+}
+
+fn print_help(keys: &Vec<(KeyCode, Action)>) {
+    for (key, action) in keys {
+        println!("{:?} => {:?}", key, action);
+    }
+}
+
+struct Position {
+    x: TrackedMemory<f32>,
+    y: TrackedMemory<f32>,
+    z: TrackedMemory<f32>,
+}
+
+impl Position {
+    fn fetch_from_game(&mut self, handle: ProcessHandle) {
+        self.x.fetch_from_game(handle);
+        self.y.fetch_from_game(handle);
+        self.z.fetch_from_game(handle);
+    }
+
+    fn apply_to_game(&mut self, handle: ProcessHandle) {
+        self.x.apply_to_game(handle);
+        self.y.apply_to_game(handle);
+        self.z.apply_to_game(handle);
+    }
+}
+
+struct TrackedMemory<T: Copy> {
+    data: T,
+    offsets: Vec<usize>,
+}
+
+impl<T: Copy> TrackedMemory<T> {
+    fn fetch_from_game(&mut self, handle: ProcessHandle) {
+        self.data = DataMember::<T>::new_offset(handle, self.offsets.clone())
+            .read()
+            .unwrap();
+    }
+
+    fn apply_to_game(&self, handle: ProcessHandle) {
+        DataMember::<T>::new_offset(handle, self.offsets.clone())
+            .write(&self.data)
+            .unwrap();
+    }
+}
 
 #[cfg(windows)]
 pub fn get_base_address(pid: Pid) -> *const u8 {
     let mut module = winapi::um::tlhelp32::MODULEENTRY32 {
-	dwSize: std::mem::size_of::<winapi::um::tlhelp32::MODULEENTRY32>() as u32,
-	th32ModuleID: 0,
-	th32ProcessID: 0,
-	GlblcntUsage: 0,
-	ProccntUsage: 0,
-	modBaseAddr: null_mut(),
-	modBaseSize: 0,
-	hModule: null_mut(),
-	szModule: [0; 256],
-	szExePath: [0; 260],
+        dwSize: std::mem::size_of::<winapi::um::tlhelp32::MODULEENTRY32>() as u32,
+        th32ModuleID: 0,
+        th32ProcessID: 0,
+        GlblcntUsage: 0,
+        ProccntUsage: 0,
+        modBaseAddr: null_mut(),
+        modBaseSize: 0,
+        hModule: null_mut(),
+        szModule: [0; 256],
+        szExePath: [0; 260],
     };
 
     let snapshot: process_memory::ProcessHandle;
@@ -79,12 +165,11 @@ pub fn get_base_address(pid: Pid) -> *const u8 {
         if winapi::um::tlhelp32::Module32First(snapshot, &mut module)
             == winapi::shared::minwindef::TRUE
         {
-	    return module.modBaseAddr;
+            return module.modBaseAddr;
         }
     }
     null()
 }
-
 
 #[cfg(windows)]
 pub fn get_pid(process_name: &str) -> Pid {
@@ -122,11 +207,10 @@ pub fn get_pid(process_name: &str) -> Pid {
                 == winapi::shared::minwindef::TRUE
             {
                 if utf8_to_string(&entry.szExeFile) == process_name {
-		    return entry.th32ProcessID;
+                    return entry.th32ProcessID;
                 }
             }
         }
     }
     0
 }
-
